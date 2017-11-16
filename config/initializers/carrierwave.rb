0ffe8cccc3afef9a7b370a0fd0fef1ab2f48f1e7
Abe Rudy <aberudy@gmail.com>
2017-11-16 14:38:37 -0500

    regaining my sanity

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,5 @@
 CarrierWave.configure do |config|
   config.root = Rails.root.join('tmp')
-  config.cache_dir = 'carrierwave'
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
@@ -8,6 +7,5 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
   config.fog_directory  = ENV["AWS_BUCKET"]              # required
-  config.s3_access_policy = :public_read
   config.fog_public = false
 end
